42adaa75c5baa87c8f427bcf41d48ab277673ae1
Christian.Martinsson <christian.martinsson@embark-studios.com>
2024-01-17 15:09:03 +0100

    Also added extra \ after \ to simulate what cmds.fileInfo is doing when writing data

--- maya_header_parser/parser_interface.py
+++ maya_header_parser/parser_interface.py
@@ -19,14 +19,16 @@ class ParserInterface(ABC):
         """
         Get file info from given file
         :param name: Name of file info
-        :param string_safe: Whether to replace " with \" in the value to make it string safe
+        :param string_safe: Whether to replace "/\ with \"/\\ in the value to make it string safe
             (the string will always be encapsulated in " in ascii files)
         :return: File info value
         """
         value = self._header_data[self.FINF].get(name)
         # Remove extra escape characters so we return same data that was sent in
-        if string_safe:
+        if value and string_safe:
+            value = value.replace("\\\\", "\\")
             value = value.replace("\\\"", "\"")
+
         return value
 
     def set_fileinfo(self, name: str, value: str, string_safe=True):
@@ -38,10 +40,16 @@ class ParserInterface(ABC):
             (the string will always be encapsulated in " in ascii files)
         :return:
         """
-        # Add extra escape character (/) around " to make sure we
+        # Add extra escape character (\) around " and \ to make sure we
         # still can read current scene even if fileInfo contains "
+        # This is also to mimic mayas own fileInfo function that adds extra \ on write
+        if not isinstance(value, str):
+            raise TypeError(f"Type {type(value)} is not supported as value, please use a string (str) instead")
+
         if string_safe:
+            value = value.replace("\\", "\\\\")
             value = value.replace("\"", "\\\"")
+
         self._header_data[self.FINF][name] = value
 
     def remove_fileinfo(self, name: str):
@@ -51,7 +59,7 @@ class ParserInterface(ABC):
     def get_all_fileinfo(self) -> dict:
         return self._header_data[self.FINF]
 
-    def get_plugin(self, name) -> str:
+    def get_plugin(self, name: str) -> str:
         return self._header_data[self.PLUG].get(name)
 
     def set_plugin(self, name: str, value: str):
